7a0ab237988f74b1221166953d9f1db19ee8cfc3
antn9x <antn9x@gmail.com>
2025-02-02 00:00:35 +0700

    fix sprite

--- src/scene/Sprite.tsx
+++ src/scene/Sprite.tsx
@@ -1,15 +1,24 @@
-import { ComponentX, LabelComp, SceneComponent, SpriteRender } from "safex";
-import { defaultFont, sf_crash } from "../assets";
+import { ButtonComp, ComponentX, LabelComp, SceneComponent, SpriteRender } from "safex";
+import { defaultFont, sf_button, sf_crash } from "../assets";
 import { BackButton } from "../components/BackButton";
 
 export class SpriteScene extends ComponentX {
+
+  sprite: SpriteRender
+
+  onPress = () => {
+    console.log("Button pressed")
+    this.sprite.spriteFrame = sf_button
+  }
 
   render() {
     return (
       <SceneComponent>
         <LabelComp node={{ x: 106, y: 240 }} string="Hello safex Sprite" font={defaultFont} />
         <BackButton />
-        <SpriteRender spriteFrame={sf_crash} />
+        <SpriteRender $ref={this.sprite} node={{ x: 500, y: 600 }} spriteFrame={sf_crash}>
+          <ButtonComp onPress={this.onPress} />
+        </SpriteRender>
       </SceneComponent>
     )
   }
