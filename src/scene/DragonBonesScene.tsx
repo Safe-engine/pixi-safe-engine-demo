1552cd9b8db651fe6cb919336fc8ef8dcda2dd31
antn1 <antn@ftech.com.vn>
2025-06-18 16:33:22 +0700

    fix db name

--- src/scene/DragonBonesScene.tsx
+++ src/scene/DragonBonesScene.tsx
@@ -1,5 +1,5 @@
 import { ComponentX, DragonBonesComp, LabelComp, SceneComponent, Vec2 } from "@safe-engine/cocos";
-import { db_mecha_1004_d, defaultFont } from "../assets";
+import { db_mecha_1004d_show, defaultFont } from "../assets";
 
 export class DragonBonesScene extends ComponentX {
 
@@ -9,7 +9,7 @@ export class DragonBonesScene extends ComponentX {
     return (
       <SceneComponent>
         <LabelComp node={{ position: Vec2(106, 240) }} string="Hello safex dragon bones" font={defaultFont} />
-        <DragonBonesComp $ref={this.dragon} node={{ position: Vec2(406, 540) }} data={db_mecha_1004_d} animation="idle" playTimes={3} />
+        <DragonBonesComp $ref={this.dragon} node={{ position: Vec2(406, 540) }} data={db_mecha_1004d_show} animation="idle" playTimes={3} />
       </SceneComponent>
     )
   }
