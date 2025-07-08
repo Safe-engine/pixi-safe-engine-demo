2a99e86b0f914da31a50f793f2cfcb23a84dbf4d
antn1 <antn@ftech.com.vn>
2025-07-08 13:25:37 +0700

    add draw

---
+++ src/scene/GraphicsScene.tsx
@@ -0,0 +1,32 @@
+import { ComponentX, GraphicsRender, LabelComp, SceneComponent, Vec2 } from '@safe-engine/cocos'
+
+import { PI_2 } from '../../packages/safex-pixi/dist'
+import { CYAN, PINK, PURPLE, YELLOW } from '../helper/constant'
+
+export class GraphicsScene extends ComponentX {
+  graphics: GraphicsRender
+
+  start() {
+    this.graphics.drawRect(Vec2(100, 300), Vec2(200, 500), PURPLE)
+    this.graphics.drawCircle(Vec2(400, 500), 50, PI_2, 64, true, 11, YELLOW)
+    const points = [
+      { x: 540, y: 1040 },
+      { x: 540, y: 1240 },
+      { x: 740, y: 1240 },
+      { x: 740, y: 1040 },
+    ].map(Vec2)
+    // this.graphics.drawPoly(points, true, BLUE)
+    const graphics = this.graphics.node.instance
+    graphics.drawPoly(points)
+    graphics.drawRect(Vec2(600, 610), Vec2(200, 345))
+  }
+
+  render() {
+    return (
+      <SceneComponent>
+        <LabelComp node={{ xy: [106, 240] }} string="Hello safex Graphics" />
+        <GraphicsRender $ref={this.graphics} fillColor={CYAN} strokeColor={PINK} lineWidth={5} />
+      </SceneComponent>
+    )
+  }
+}
