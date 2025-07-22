3ae3d634c7e9a45bffebfdbf756afb2457d8a3d7
antn1 <antn@ftech.com.vn>
2025-07-22 17:38:29 +0700

    use promise

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,4 @@
-import { GameWorld, initWorld, loadAll, loadScene, setupCollider, setupDragonBones, setupSpine, startGame } from '@safe-engine/cocos'
+import { GameWorld, loadAll, loadScene, setupCollider, setupDragonBones, setupSpine, startGame } from '@safe-engine/cocos'
 import { initBox2d, setupPhysics } from '@safe-engine/cocos/dist/box2d-wasm'
 
 import { defaultFont, sf_progress_bar } from './assets'
@@ -10,25 +10,13 @@ import { colliderMatrix, designedResolution } from './settings'
 //     cc.game.onStart()
 //   })
 // }
-initBox2d(() => {
-  startGame(
-    {
-      debugMode: 1,
-      showFPS: false,
-      frameRate: 60,
-      id: 'gameCanvas',
-      renderMode: 1,
-    },
-    designedResolution,
-    () => {
-      initWorld(defaultFont)
-      setupDragonBones(GameWorld.Instance)
-      setupSpine(GameWorld.Instance)
-      setupCollider(colliderMatrix, true)
-      setupPhysics(GameWorld.Instance, true)
-      loadAll([sf_progress_bar], (p) => {
-        if (p >= 1) loadScene(Loading)
-      })
-    },
-  )
+initBox2d().then(async () => {
+  await startGame(defaultFont, designedResolution)
+  setupDragonBones(GameWorld.Instance)
+  setupSpine(GameWorld.Instance)
+  setupCollider(colliderMatrix, true)
+  setupPhysics(GameWorld.Instance, true)
+  loadAll([sf_progress_bar], (p) => {
+    if (p >= 1) loadScene(Loading)
+  })
 })
