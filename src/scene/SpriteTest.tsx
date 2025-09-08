fcbd5a0e731f837cb8d60ed0a6678e546b8058a6
antn1 <antn@ftech.com.vn>
2025-09-08 15:50:23 +0700

    test tiled

--- src/scene/SpriteTest.tsx
+++ src/scene/SpriteTest.tsx
@@ -1,4 +1,4 @@
-import { ComponentX, SceneComponent, SpriteRender } from '@safe-engine/cocos'
+import { ComponentX, SceneComponent, Size, SpriteRender } from '@safe-engine/cocos'
 
 import { sf_crash, sf_dialog_name } from '../assets'
 import { BackButton } from '../components/BackButton'
@@ -14,8 +14,9 @@ export default class SpriteTest extends ComponentX {
         {Array(4).map((i = 1) => (
           <SpriteRender spriteFrame={sf_crash} node={{ xy: [115, 350 + i * 275], scale: 1.48 }} />
         ))}
+        <SpriteRender spriteFrame={sf_crash} node={{ xy: [500, 500], contentSize: Size(100, 600) }} isTiling={true}></SpriteRender>
         <SpriteRender spriteFrame={sf_crash} node={{ xy: [300, 300] }}>
-          <SpriteRender spriteFrame={sf_dialog_name}></SpriteRender>
+          <SpriteRender spriteFrame={sf_dialog_name} isTiling={true} node={{ contentSize: Size(400, 600) }}></SpriteRender>
         </SpriteRender>
         <BackButton />
       </SceneComponent>
