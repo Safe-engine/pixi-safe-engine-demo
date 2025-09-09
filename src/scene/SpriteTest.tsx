dc1f2ba44c59ce7fbeffacc739025a5da48467ea
antn1 <antn@ftech.com.vn>
2025-09-09 15:09:31 +0700

    test tiled

--- src/scene/SpriteTest.tsx
+++ src/scene/SpriteTest.tsx
@@ -12,19 +12,15 @@ export default class SpriteTest extends ComponentX {
     return (
       <SceneComponent>
         {Array(4).map((i = 1) => (
-          <SpriteRender spriteFrame={sf_crash} node={{ xy: [115, 350 + i * 275], scale: 1.48 }} />
+          <SpriteRender spriteFrame={sf_crash} node={{ xy: [115, 350 + i * 275] }} />
         ))}
         <SpriteRender
           spriteFrame={sf_dialog_name}
           node={{ xy: [500, 500], contentSize: Size(200, 600) }}
-          capInsets={cc.rect(10, 10, 20, 10)}
+          capInsets={[10, 10, 20, 10]}
           type={SpriteTypes.SLICED}
         ></SpriteRender>
-        <SpriteRender
-          spriteFrame={sf_crash}
-          type={SpriteTypes.FILLED}
-          node={{ xy: [300, 300], contentSize: Size(400, 600) }}
-        ></SpriteRender>
+        <SpriteRender spriteFrame={sf_crash} type={SpriteTypes.TILED} tiledSize={Size(320, 1600)} node={{ xy: [330, 1600] }}></SpriteRender>
         <BackButton />
       </SceneComponent>
     )
