e1d3226a2930c67137bccb35ef0716306e68dff2
antn1 <antn@ftech.com.vn>
2025-11-03 11:16:56 +0700

    fix load

--- src/binding/loader.ts
+++ src/binding/loader.ts
@@ -1,45 +1,13 @@
-import { Assets, extensions, Texture } from '@safe-engine/pixi'
+import { Assets, extensions, loadAll } from '@safe-engine/pixi'
 import HowlerLoaderParser from 'howler-pixi-loader-middleware'
 
-import * as AudioAssets from '../assets/AudioAssets'
-import * as DragonBonesAssets from '../assets/DragonBonesAssets'
-import * as FontAssets from '../assets/FontAssets'
-import * as SpineAssets from '../assets/SpineAssets'
-import * as TextureAssets from '../assets/TextureAssets'
+import * as allAssets from '../assets'
 
 extensions.add(HowlerLoaderParser)
 
-export function loadAssets(cb: (progress: number) => void, onCompleted: () => void) {
-  // load the texture we need
-  const fontBundle = {}
-  Object.keys(FontAssets).forEach((key) => {
-    const val = FontAssets[key]
-    fontBundle[val] = val
-  })
-  Assets.addBundle('fonts', fontBundle)
-  const keys = []
-  Object.keys(SpineAssets).map((key) => {
-    const { skeleton, atlas } = SpineAssets[key]
-    Assets.add({ alias: skeleton, src: skeleton })
-    Assets.add({ alias: atlas, src: atlas })
-    keys.push(skeleton, atlas)
-  })
-  Object.keys(AudioAssets).map((key) => {
-    keys.push(AudioAssets[key])
-  })
-  Object.values(DragonBonesAssets).map((value) => {
-    // console.log(key, value)
-    const { skeleton, atlas, texture } = value
-    keys.push(skeleton, atlas, texture)
-  })
-  return Promise.all([
-    Assets.loadBundle('fonts'),
-    Assets.load(keys),
-    // ...Object.values(JsonAssets).map(loadJsonAsync),
-  ]).then(async () => {
-    await Assets.load<Texture>(Object.values(TextureAssets), cb)
-    onCompleted()
-  })
+export async function loadAssets(cb: (progress: number) => void, onCompleted: () => void) {
+  await loadAll(allAssets, cb)
+  onCompleted()
 }
 
 const jsonCache = {}
