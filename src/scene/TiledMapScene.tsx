917184d51060d0039c1615ba68b5823dead5b2e3
antn1 <antn@ftech.com.vn>
2025-11-06 16:22:37 +0700

    fix get pos

--- src/scene/TiledMapScene.tsx
+++ src/scene/TiledMapScene.tsx
@@ -1,16 +1,27 @@
-import { ComponentX, LabelComp, SceneComponent } from '@safe-engine/pixi'
+import { ComponentX, LabelComp, SceneComponent, SpriteRender, Vec2 } from '@safe-engine/pixi'
 
 import { TiledMapComp } from '@safe-engine/pixi/dist/tiledmap/TiledMapComp'
-import { map_1_json } from '../assets'
+import { map_1_json, sf_crash } from '../assets'
 import { BackButton } from '../components/BackButton'
 
 export class TiledMapScene extends ComponentX {
+  tiledMapComp: TiledMapComp
+  spriteRender: SpriteRender
+
+  start() {
+    // console.log('TiledMapScene started', this.tiledMapComp.node.instance)
+    const { x, y } = this.tiledMapComp.getPositionAt(4, 7)
+    this.spriteRender.node.position = Vec2(x, y)
+  }
+
   render() {
     return (
       <SceneComponent>
         <LabelComp node={{ xy: [540, 140] }} string="Hello safex tiled" />
         <BackButton />
-        <TiledMapComp node={{ xy: [306, 140] }} mapUrl={map_1_json} />
+        <TiledMapComp $ref={this.tiledMapComp} node={{ xy: [306, 140] }} mapFile={map_1_json}>
+          <SpriteRender $ref={this.spriteRender} spriteFrame={sf_crash}></SpriteRender>
+        </TiledMapComp>
       </SceneComponent>
     )
   }
