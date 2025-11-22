de4fa9dd0e6482a6aad303fa7764713f1cf056c1
antn9x <antn9x@gmail.com>
2025-11-23 01:31:04 +0700

    add node action

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,3 +1,5 @@
+import { Action, Animation } from 'pixi-action-ease'
+
 interface HMR {
   accept: (cb?: () => void) => void
   dispose: (cb?: () => void) => void
@@ -5,4 +7,15 @@ interface HMR {
 
 declare namespace module {
   const hot: HMR
+}
+
+// Add this after the imports (after importing Action and Animation)
+declare module '@safe-engine/pixi' {
+  interface NodeComp {
+    actionsList: Animation[]
+    runAction(act: Action): Animation
+    stopAllActions(): void
+    pauseAllActionsAndSchedule(): void
+    resumeAllActionsAndSchedule(): void
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Assets, loadScene, startGame, Texture, Vec2 } from '@safe-engine/pixi'
+import { Assets, loadScene, NodeComp, startGame, Texture, Vec2 } from '@safe-engine/pixi'
 
 import { initBox2d, setupPhysics } from '@safe-engine/pixi/dist/box2d-wasm'
 import { setupCollider } from '@safe-engine/pixi/dist/collider'
@@ -6,22 +6,45 @@ import { setupDragonBones } from '@safe-engine/pixi/dist/dragonbones'
 import { setupGUI } from '@safe-engine/pixi/dist/gui'
 import { setupRichText } from '@safe-engine/pixi/dist/richtext'
 import { setupSpine } from '@safe-engine/pixi/dist/spine'
-import { setupTiledMap } from '@safe-engine/pixi/dist/tiledmap'
+import { actionManager, Animation } from 'pixi-action-ease'
 import { defaultFont, sf_progress_bar, sf_progress_bg } from './assets'
 import { Loading } from './scene/Loading'
 import { colliderMatrix, designedResolution } from './settings'
 
 async function start() {
   await initBox2d()
-  await startGame(defaultFont, designedResolution, Assets)
+  const app = await startGame(defaultFont, designedResolution, Assets)
   setupGUI()
   setupRichText()
   setupSpine()
   setupDragonBones()
   setupCollider(colliderMatrix, true)
   setupPhysics(colliderMatrix, true, Vec2(0, 98))
-  setupTiledMap()
   await Assets.load<Texture>([sf_progress_bar, sf_progress_bg])
   loadScene(Loading)
+  app.ticker.add(() => {
+    const dt = app.ticker.deltaMS * 0.001
+    actionManager.update(dt)
+  })
+
+  NodeComp.prototype.actionsList = []
+  NodeComp.prototype.stopAllActions = function () {
+    this.actionsList.forEach((act) => {
+      actionManager.cancelAction(act)
+    })
+    this.actionsList = []
+  }
+
+  NodeComp.prototype.pauseAllActionsAndSchedule = function () {
+    this.actionsList.forEach((anim: Animation) => {
+      anim.isPause = true
+    })
+  }
+
+  NodeComp.prototype.resumeAllActionsAndSchedule = function () {
+    this.actionsList.forEach((anim: Animation) => {
+      anim.isPause = false
+    })
+  }
 }
 start()
